5ccf4b3ff6977d685173390247ef5880a1709581
John Detlefs <jdetle@gmail.com>
2019-05-28 16:16:48 -0500

    Fix use callback example to show of the powah

--- src/routes/use-callback.tsx
+++ src/routes/use-callback.tsx
@@ -1,23 +1,25 @@
 import React, { useCallback, useState, useEffect } from "react";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 type ParentProps = { userDataEndpoint: string };
 
 const fetch = (url: string) => Promise.resolve("data url:   " + url);
-
-export const Parent = (props: ParentProps) => {
+const DataDisplay = ({ fetchData }: { fetchData: () => Promise<string> }) => {
   const [data, setData] = useState<string>("No data url set yet");
 
+  useEffect(() => {
+    fetchData().then(setData);
+  }, [fetchData]);
+  return <Typography>{data}</Typography>;
+};
+export const Parent = (props: ParentProps) => {
   const fetchData = useCallback(() => {
-    fetch("api/url" + props.userDataEndpoint).then(setData);
+    return fetch("api/url" + props.userDataEndpoint);
   }, [props.userDataEndpoint]);
 
   return (
     <div>
-      <Typography>{data}</Typography>
-      <Button onClick={fetchData}>Fetch Data</Button>
-      <Button onClick={() => setData("")}>Clear Data</Button>
+      <DataDisplay fetchData={fetchData} />
     </div>
   );
 };
@@ -29,7 +31,7 @@ export const GrandParent = (props: {}) => {
     setTimeout(() => {
       setUserDataEndpoint("?id=" + count);
       setCount(count => count + 1);
-    }, 3000);
+    }, 2000);
   }, [count]);
   return (
     <div style={{ margin: 20, padding: 20 }}>
